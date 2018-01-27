3f3543f687b4a3bcd590f71f26c35d581720a874
Hörður Ragnarsson <hordur.ragnarsson@gmail.com>
2018-01-27 18:06:40 +0000

    Adding lightbox controls, ES6 compiler

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'sass'
 gem 'jekyll'
+gem 'jekyll-babel'
 
 group :jekyll_plugins do
     gem 'jekyll-livereload'
--- js/script.js
+++ js/script.js
@@ -1,1 +1,66 @@
-//custom js goes here
+---
+---
+
+/* CLASSES */
+class Gallery {
+    constructor () {}
+
+    static close () {
+        $('.fullscreen, .fullscreen img').removeClass('show');
+    }
+
+    static showBefore () {
+        let newImage = $('.fullscreen img.show').prev('img');
+        if (newImage.length !== 1) {
+            newImage = $('.fullscreen img').last();
+        }
+        $('.fullscreen img').removeClass('show');
+        $(newImage).addClass('show');
+    }
+
+    static showNext () {
+        let newImage = $('.fullscreen img.show').next('img');
+        if (newImage.length !== 1) {
+            newImage = $('.fullscreen img').first();
+        }
+        $('.fullscreen img').removeClass('show');
+        $(newImage).addClass('show');
+    }
+
+    static showThis(target) {
+        let index = $(target).index();
+        $('.fullscreen, .fullscreen img:eq(' + index + ')').addClass('show');
+    }
+}
+
+/* EVENTS */
+/* Fullcreen trigger */
+$('.gallery img').on('click', (event) => {
+    Gallery.showThis(event.target);
+});
+/* Fullscreen navigation */
+$('.fullscreen .close').on('click', Gallery.close);
+$('.fullscreen .before').on('click', Gallery.showBefore);
+$('.fullscreen .next').on('click', Gallery.showNext);
+$('.fullscreen').on('click', (event) => {
+    if ($(event.target).has('img').length) {
+        Gallery.close();
+    }
+});
+$(document).on('keyup', (event) => {
+    console.log(event.keyCode);
+    switch (event.keyCode) {
+    case 27:
+        /* ESC key */
+        Gallery.close();
+        break;
+    case 37:
+        /* Left arrow key */
+        Gallery.showBefore();
+        break;
+    case 39:
+        /* Right arrow key */
+        Gallery.showNext();
+        break;
+    }
+});
